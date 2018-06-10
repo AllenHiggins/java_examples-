7c2d020bd8d9d261fb458c38d881a460258c5140
Allen Higgins <32021033+AllenHiggins@users.noreply.github.com>
2018-06-10 15:03:28 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,11 +5,21 @@ public class Main {
     /*
     * Allen Higgins
     * 09/June/2018
-    * Small programme to check if a word is a palindrome
-    * Note: A palindrome word is not the same as a palindrome sentence 
+    *
+    * Part 1:
+    * Check if a word is a palindrome
+    * Note: A palindrome word is not the same as a palindrome sentence
+    *
+    * Part 2:
+    * Count how may digits of a number divide into that number with remainder of zero
+    *
+    * Part 3:
+    * Swap two int values, one using a temp and another without using a temp
     * */
     public static void main(String[] args) {
-
+        /*
+        * Part 1:
+        * */
         String word = "Rotator";
         /*
         *  Case sensitive
@@ -41,5 +51,86 @@ public class Main {
             }
         }
 
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println();
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+        * Part 2:
+        * Count how may digits of a number divide into that number with remainder of zero
+        *   --> convert number to string
+        *   --> loop through the string
+        *           --> for each char get its int value
+        *           --> if the number mod the current digit equals zero count it
+        * */
+        int n = 453000312, count = 0, num = 0;
+        String numStr = Integer.toString(n);
+
+        for(int i = 0; i <= numStr.length()-1;i++){
+            num = Integer.parseInt(String.valueOf(numStr.charAt(i)));
+            try{
+                if( n % num == 0){
+                    count++;
+                }
+            }catch (ArithmeticException e){
+                // Divide by zero error
+                System.out.println("Exception Handled -->> " + e);
+            }
+        }
+        System.out.println("The divide count of " + n + " by it's singal digits is: " + count);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println();
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+        * Part 3:
+        * Swap two int values, one using a temp and another without using a temp
+        * */
+        int a = 5, b = 6;
+        swapWithtemp(a,b);
+        swapWithoutTemp(a,b);
     }
+    /*
+    * Part 3
+    * swapWithtemp(int a, int b)
+    *    --> Using a temp var
+    * */
+    public static void swapWithtemp(int a, int b){
+        printValBeforeSwap(a, b);
+        int temp = 0;
+        temp = a;
+        a = b;
+        b = temp;
+        printValAfterSwap(a,b);
+    }
+    /*
+    * Part 3
+    * swapWithoutTemp(int a, int b)
+    *    --> Using no temp var
+    * */
+    public static void swapWithoutTemp(int a, int b){
+        printValBeforeSwap(a, b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        printValAfterSwap(a,b);
+    }
+    /*
+    * Part 3
+    * Print statements of values before swap
+    * */
+    public static void printValBeforeSwap(int a, int b){
+        System.out.println("(a) was: " + a + " and (b) was:" + b);
+    }
+    /*
+     * Part 3
+     * Print statements of values after swap
+     * */
+    public static void printValAfterSwap(int a, int b){
+        System.out.println( "(a) is now: " + a + " and (b) is now: " + b + " after swapping");
+    }
+
+
+
 }
